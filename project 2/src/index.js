b1b1ef1278c35e4c76dc22f022f021b2bdf58a59
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-10-08 23:46:25 +0600

    Update index.js

--- project 2/src/index.js
+++ project 2/src/index.js
@@ -4,32 +4,48 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const title ="Calling me";
-const date = new Date();
-const dateName = date.getDate();
-const monthName = date.getMonth();
-const yearName = date.getFullYear();
 
-// Create Componant
-function Card(){
-  return <div className='card'>
-      <h3 className='cardTitle'>{title}</h3>
-      <p className='cardDes'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, at!</p>
-      <p className='cardFooter'>{dateName + " " + monthName + " " + yearName}</p>
-    </div>
-}
 
 createRoot(document.getElementById('root')).render(
   <div>
-    <h1 className='headingStyle'>Hello React!</h1>
-
-{/* Useing COmponant */}
-    <Card />
-    <Card />
-    <Card />
-    
-
-     
+    <App />
   </div>
 
 )
+
+
+// import React from 'react';
+// import ReactDOM, {createRoot} from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const title ="Calling me";
+// const date = new Date();
+// const dateName = date.getDate();
+// const monthName = date.getMonth();
+// const yearName = date.getFullYear();
+
+// // Create Componant
+// function Card(){
+//   return <div className='card'>
+//       <h3 className='cardTitle'>{title}</h3>
+//       <p className='cardDes'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, at!</p>
+//       <p className='cardFooter'>{dateName + " " + monthName + " " + yearName}</p>
+//     </div>
+// }
+
+// createRoot(document.getElementById('root')).render(
+//   <div>
+//     <h1 className='headingStyle'>Hello React!</h1>
+
+// {/* Useing COmponant */}
+//     <Card />
+//     <Card />
+//     <Card />
+    
+
+     
+//   </div>
+
+// )
